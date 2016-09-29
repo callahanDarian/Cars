f71101b5fa9abbf88ae9f3fdd59bf65e5abe94b0
Darian C <ectomoshi@yahoo.com>
2016-09-29 10:35:28 -0500

    Add files via upload

---
+++ Cars/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace Cars
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Car1 = new System.Windows.Forms.PictureBox();
+            this.Car2 = new System.Windows.Forms.PictureBox();
+            this.Car3 = new System.Windows.Forms.PictureBox();
+            this.Car4 = new System.Windows.Forms.PictureBox();
+            this.Car5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblOne = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.lblYellow = new System.Windows.Forms.Label();
+            this.lblOrange = new System.Windows.Forms.Label();
+            this.lblBlue = new System.Windows.Forms.Label();
+            this.lblGreen = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Car1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Car1
+            // 
+            this.Car1.BackColor = System.Drawing.Color.Red;
+            this.Car1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Car1.BackgroundImage")));
+            this.Car1.Location = new System.Drawing.Point(84, 13);
+            this.Car1.Name = "Car1";
+            this.Car1.Size = new System.Drawing.Size(100, 50);
+            this.Car1.TabIndex = 0;
+            this.Car1.TabStop = false;
+            // 
+            // Car2
+            // 
+            this.Car2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Car2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Car2.BackgroundImage")));
+            this.Car2.Location = new System.Drawing.Point(84, 69);
+            this.Car2.Name = "Car2";
+            this.Car2.Size = new System.Drawing.Size(100, 50);
+            this.Car2.TabIndex = 1;
+            this.Car2.TabStop = false;
+            // 
+            // Car3
+            // 
+            this.Car3.BackColor = System.Drawing.Color.Yellow;
+            this.Car3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Car3.BackgroundImage")));
+            this.Car3.Location = new System.Drawing.Point(84, 125);
+            this.Car3.Name = "Car3";
+            this.Car3.Size = new System.Drawing.Size(100, 50);
+            this.Car3.TabIndex = 2;
+            this.Car3.TabStop = false;
+            // 
+            // Car4
+            // 
+            this.Car4.BackColor = System.Drawing.Color.Lime;
+            this.Car4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Car4.BackgroundImage")));
+            this.Car4.Location = new System.Drawing.Point(84, 181);
+            this.Car4.Name = "Car4";
+            this.Car4.Size = new System.Drawing.Size(100, 50);
+            this.Car4.TabIndex = 3;
+            this.Car4.TabStop = false;
+            // 
+            // Car5
+            // 
+            this.Car5.BackColor = System.Drawing.Color.Aqua;
+            this.Car5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Car5.BackgroundImage")));
+            this.Car5.Location = new System.Drawing.Point(84, 237);
+            this.Car5.Name = "Car5";
+            this.Car5.Size = new System.Drawing.Size(100, 50);
+            this.Car5.TabIndex = 4;
+            this.Car5.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(193, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(17, 296);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(734, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(17, 296);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Scoot";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(259, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 108);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(259, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(262, 108);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Wins";
+            this.label2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(55, 58);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Reset Cars";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblOne
+            // 
+            this.lblOne.AutoSize = true;
+            this.lblOne.Location = new System.Drawing.Point(907, 9);
+            this.lblOne.Name = "lblOne";
+            this.lblOne.Size = new System.Drawing.Size(38, 13);
+            this.lblOne.TabIndex = 11;
+            this.lblOne.Text = "Score:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(872, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Orange:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(886, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Blue:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(878, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Green:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(876, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Yellow:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(887, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Red:";
+            // 
+            // lblRed
+            // 
+            this.lblRed.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblRed.Location = new System.Drawing.Point(923, 50);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(100, 23);
+            this.lblRed.TabIndex = 17;
+            // 
+            // lblYellow
+            // 
+            this.lblYellow.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblYellow.Location = new System.Drawing.Point(923, 96);
+            this.lblYellow.Name = "lblYellow";
+            this.lblYellow.Size = new System.Drawing.Size(100, 23);
+            this.lblYellow.TabIndex = 18;
+            // 
+            // lblOrange
+            // 
+            this.lblOrange.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOrange.Location = new System.Drawing.Point(923, 72);
+            this.lblOrange.Name = "lblOrange";
+            this.lblOrange.Size = new System.Drawing.Size(100, 23);
+            this.lblOrange.TabIndex = 19;
+            // 
+            // lblBlue
+            // 
+            this.lblBlue.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblBlue.Location = new System.Drawing.Point(923, 142);
+            this.lblBlue.Name = "lblBlue";
+            this.lblBlue.Size = new System.Drawing.Size(100, 23);
+            this.lblBlue.TabIndex = 20;
+            // 
+            // lblGreen
+            // 
+            this.lblGreen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblGreen.Location = new System.Drawing.Point(923, 119);
+            this.lblGreen.Name = "lblGreen";
+            this.lblGreen.Size = new System.Drawing.Size(100, 23);
+            this.lblGreen.TabIndex = 21;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(55, 58);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Reset Score";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1036, 306);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lblGreen);
+            this.Controls.Add(this.lblBlue);
+            this.Controls.Add(this.lblOrange);
+            this.Controls.Add(this.lblYellow);
+            this.Controls.Add(this.lblRed);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblOne);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Car5);
+            this.Controls.Add(this.Car4);
+            this.Controls.Add(this.Car3);
+            this.Controls.Add(this.Car2);
+            this.Controls.Add(this.Car1);
+            this.Name = "Form1";
+            this.Text = "Cars";
+            ((System.ComponentModel.ISupportInitialize)(this.Car1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Car1;
+        private System.Windows.Forms.PictureBox Car2;
+        private System.Windows.Forms.PictureBox Car3;
+        private System.Windows.Forms.PictureBox Car4;
+        private System.Windows.Forms.PictureBox Car5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblOne;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblRed;
+        private System.Windows.Forms.Label lblYellow;
+        private System.Windows.Forms.Label lblOrange;
+        private System.Windows.Forms.Label lblBlue;
+        private System.Windows.Forms.Label lblGreen;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Cars/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cars
+{
+    public partial class Form1 : Form
+      
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            int carPos1 = 50;
+            int carPos2 = 50;
+            int carPos3 = 50;
+            int carPos4 = 50;
+            int carPos5 = 50;
+            int red, orange, yellow, blue, green;
+
+        private void button1_Click(object sender, EventArgs e)
+          {
+                Random i = new Random();
+
+                int car1Speed1 = i.Next(5, 50);
+                int car2Speed2 = i.Next(5, 50);
+                int car3Speed3 = i.Next(5, 50);
+                int car4Speed4 = i.Next(5, 50);
+                int car5Speed5 = i.Next(5, 50);
+
+                carPos1 = carPos1 + car1Speed1;
+                carPos2 = carPos2 + car2Speed2;
+                carPos3 = carPos3 + car3Speed3;
+                carPos4 = carPos4 + car4Speed4;
+                carPos5 = carPos5 + car5Speed5;
+
+                this.Car1.Left = this.Car1.Left + car1Speed1;
+                this.Car2.Left = this.Car2.Left + car2Speed2;
+                this.Car3.Left = this.Car3.Left + car3Speed3;
+                this.Car4.Left = this.Car4.Left + car4Speed4;
+                this.Car5.Left = this.Car5.Left + car5Speed5;
+
+            if (carPos1 >= 700)
+            {
+                label1.Text = "Red";
+
+                label1.Visible = true;
+                label2.Visible = true;
+                red = +1;
+
+                button1.Visible = false;
+            }
+            if (carPos2 >= 700)
+            {
+                label1.Text = "Orange";
+
+                label1.Visible = true;
+                label2.Visible = true;
+                orange = +1;
+
+                button1.Visible = false;
+            }
+            if (carPos3 >= 700)
+            {
+                label1.Text = "Yellow";
+
+                label1.Visible = true;
+                label2.Visible = true;
+                yellow = +1;
+
+                button1.Visible = false;
+            }
+            if (carPos4 >= 700)
+            {
+                label1.Text = "Green";
+
+                label1.Visible = true;
+                label2.Visible = true;
+                green = +1;
+
+                button1.Visible = false;
+            }
+            if (carPos5 >= 700)
+            {
+                label1.Text = "Blue";
+
+                label1.Visible = true;
+                label2.Visible = true;
+                blue = +1;
+
+                button1.Visible = false;
+            }
+
+            lblRed.Text = " " + red;
+            lblOrange.Text = " " + orange;
+            lblYellow.Text = " " + yellow;
+            lblGreen.Text = " " + green;
+            lblBlue.Text = " " + blue;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Car1.Left = 84;
+            this.Car2.Left = 84;
+            this.Car3.Left = 84;
+            this.Car4.Left = 84;
+            this.Car5.Left = 84;
+
+            carPos1 = 50;
+            carPos2 = 50;
+            carPos3 = 50;
+            carPos4 = 50;
+            carPos5 = 50;
+
+            button1.Visible = true;
+            label1.Visible = false;
+            label2.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            red = 0;
+            orange = 0;
+            yellow = 0;
+            green = 0;
+            blue = 0;
+
+            lblRed.Text = " " + red;
+            lblOrange.Text = " " + orange;
+            lblYellow.Text = " " + yellow;
+            lblGreen.Text = " " + green;
+            lblBlue.Text = " " + blue;
+        }
+    }
+}
